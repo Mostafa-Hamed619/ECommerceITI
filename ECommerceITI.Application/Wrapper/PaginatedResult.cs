899f83b34344b1dc4dbfd77678cdb3234ce4c219
Mostafa-Hamed619 <hamedmostafa726@gmail.com>
2025-06-04 19:21:04 +0300

    Build The Clean Architecture

---
+++ ECommerceITI.Application/Wrapper/PaginatedResult.cs
@@ -0,0 +1,56 @@
+﻿namespace ECommerceITI.Application.Wrapper
+{
+    public class PaginatedResult<T>
+    {
+        public List<T> Data { get; set; }
+
+        public PaginatedResult(List<T> data)
+        {
+            Data = data;
+        }
+
+        public PaginatedResult(bool succeeded, List<T> data = default, List<string> messages = null, int count = 0, int page = 1, int pageSize = 10)
+        {
+            Data = data;
+            CurrentPage = page;
+            Succeeded = succeeded;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            TotalCount = count;
+        }
+
+        public static PaginatedResult<T> Success(List<T> data, int count, int page, int pageSize)
+        {
+            return new(true, data, null, count, page, pageSize);
+        }
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public object Meta { get; set; }
+
+        public int PageSize { get; set; }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return CurrentPage > 1;
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return CurrentPage < TotalPages;
+            }
+        }
+
+        public List<string> Messages { get; set; } = new();
+
+        public bool Succeeded { get; set; }
+    }
+}
