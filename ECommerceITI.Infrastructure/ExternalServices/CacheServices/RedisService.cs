8e87904e8a409a0476d31fdd8cfa3475fd80af93
Mostafa-Hamed619 <hamedmostafa726@gmail.com>
2025-06-04 21:45:47 +0300

    Create Docker file - Redis Services

---
+++ ECommerceITI.Infrastructure/ExternalServices/CacheServices/RedisService.cs
@@ -0,0 +1,37 @@
+﻿using ECommerceITI.Domain.Interfaces.Caching;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+using System.Text.Json;
+
+namespace ECommerceITI.Infrastructure.ExternalServices.CacheServices
+{
+    public class RedisService : IRedisService
+    {
+        private readonly IDistributedCache? _cache;
+
+        public RedisService(IDistributedCache? cache)
+        {
+            _cache = cache;
+        }
+
+        public T? GetData<T>(string Key)
+        {
+            var data = _cache?.GetString(Key);
+
+            if (data is null)
+                return default(T);
+
+            return JsonSerializer.Deserialize<T>(data);
+        }
+
+        public void SetData<T>(string Key, T Data)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1)
+            };
+
+            _cache?.SetString(key: Key, value: JsonSerializer.Serialize(Data), options);
+        }
+    }
+}
